7bd6435404556e8b88c88ec9df6a6c58913191ce
Nawaz Gafar <gafar.nawaz@gmail.com>
2023-05-05 00:48:32 -0400

    Renamed

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os 
-from render import render
+from render import concat
 
-'''
+
 videos_dir = './videos'
 video_files = os.listdir(videos_dir)
 
 with open('input.txt', 'w') as f:
     for video_file in video_files:
         f.write(f"file '{videos_dir}/{video_file}'\n")
-'''
 
-render.concat_videos()
+
+concat.concat_videos()
